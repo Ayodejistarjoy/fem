ccc3fe84fa14da61e25c9d82b7821ecae080c5df
AYODEJI <ayodejialao0@gmail.com>
2023-12-20 15:07:50 +0100

    my commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function press() {
+    
+     pic.style.display="block"
+    trd.style.display = "block"
+     me.style.display = "none"
+    cord.style.backgroundImage = "none"
+    need.style.display= "inline-block"
+    nee.style.display= "inline-block"
+    need.style.margin="40px"
+
+ }
+
+var dance ="cal.mp3"
+var mad = new Audio(dance)
+function sand() {
+    mad.play()
+}
+function beat() {
+    mad.pause()
+    
+}
